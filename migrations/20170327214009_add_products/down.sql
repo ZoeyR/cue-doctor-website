e6428f06a1e6861531f08fbfc4f3b4e9e4582c48
Daniel Griffen <daniel@griffen.io>
2017-04-01 19:50:51 -0500

    Add product migrations

---
+++ migrations/20170327214009_add_products/down.sql
@@ -0,0 +1,11 @@
+DELETE FROM products
+WHERE name='Cue Wax';
+
+DELETE FROM products
+WHERE name='Shaft Cleaner';
+
+DELETE FROM products
+WHERE name='Dent Buffer';
+
+DELETE FROM products
+WHERE name='Magic Cloth';
