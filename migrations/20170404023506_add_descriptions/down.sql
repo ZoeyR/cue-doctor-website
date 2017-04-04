169ad8ea4d5f78c18bfbde84ce01b1ee2a4eae59
Daniel Griffen <daniel@griffen.io>
2017-04-03 21:47:09 -0500

    Add description migration

---
+++ migrations/20170404023506_add_descriptions/down.sql
@@ -0,0 +1,15 @@
+UPDATE products
+SET description = 'TODO'
+WHERE name = 'Cue Wax';
+
+UPDATE products
+SET description = 'TODO'
+WHERE name = 'Shaft Cleaner';
+
+UPDATE products
+SET description = 'TODO'
+WHERE name = 'Dent Buffer';
+
+UPDATE products
+SET description = 'TODO'
+WHERE name = 'Magic Cloth';
---
+++ migrations/20170404023506_add_descriptions/up.sql
@@ -0,0 +1,16 @@
+-- Your SQL goes here
+UPDATE products
+SET description = 'Cue Doctor Wax is a breakthrough formula that combines ease of use with ultimate protection and a silky-smooth feel. It is a blend of pure Carnauba wax with silicones and other waxes that provides longer-lasting protection without "build-up". It is specially designed for cue shafts, after using CUE DOCTOR CUE SHAFT CLEANER & CONDITIONER, but is equally effective on urethane and polymer-coated portions of the cue. When used regularly this wax repels moisture and dirt, thus keeping the cue looking and feeling like new, and playing great!'
+WHERE name = 'Cue Wax';
+
+UPDATE products
+SET description = 'The Cue Doctor Shaft Cleaner & Conditioner is an exclusive laboratory-tested formula which not only cleans but conditions the wood in one easy application. Unlike some cleaners, it has no alcohol, acetone, or other harsh chemicals that can dry out the wood and damage a cue shaft. Instead, it is micro-targeted to gently lift out dirt and chalk residue while nourishing the wood and leaving it with a soft sheen. It is best used with cue CUE DOCTOR WAX for ultimate protection and a genuinely smooth stroke.'
+WHERE name = 'Shaft Cleaner';
+
+UPDATE products
+SET description = 'The Cue Doctor Dent Buffer has caused quite a positive stir, and it is well deserved. Every cue can acquire those annoying nicks and dents in the shaft that can affect total concentration. This tool is simple, convenient, and effective for removing those dents, without damaging the wood with steam or alcohol. It is made of hardened borosilicate glass to assure the utmost in quality and consistency. Simply rub the glass rod over the dent in the shaft lightly and vigorously until the clicking sound of the dent fades away, as the dent is smoothed. This product can also be used to shine your worn Irish linen wraps, or smooth out frays and scratches. It has repeatedly gotten high ratings and rave reviews, and is well worth every player''s consideration.'
+WHERE name = 'Dent Buffer';
+
+UPDATE products
+SET description = 'The Cue Doctor Cloth is the ultimate improvement on cloths which didn''t fill the needs of discriminating players. It gives your cue the lustrous, lasting protection it needs, and reduces "drag" in the bridge hand. This soft, double-napped cue cloth is impregnated with pure silicone and won''t dry out, stain, or smell like other cloths on the market.'
+WHERE name = 'Magic Cloth';
