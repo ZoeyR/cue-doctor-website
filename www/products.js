8da88d1d0498275cb7e565a8429f4a6b5ce3d199
Daniel Griffen <daniel@griffen.io>
2017-04-03 21:47:17 -0500

    Fix description display

--- www/products.js
+++ www/products.js
@@ -1,48 +1,47 @@
 //url: /products
 //returns json that is array of products
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-// the code is inside the ready function - 
-// makes sure that DOM is loaded before using jQuery
+    // the code is inside the ready function - 
+    // makes sure that DOM is loaded before using jQuery
 
-if(!sessionStorage.order && !sessionStorage.count)
-{
-    sessionStorage.count = 0;
-}
+    if (!sessionStorage.order && !sessionStorage.count) {
+        sessionStorage.count = 0;
+    }
 
-$("#cartcount").text(sessionStorage.count);
+    $("#cartcount").text(sessionStorage.count);
 
-  //alert("Now going to make POST ajax call - results will appear soon");
-$.get("/products", function(data,status) {
+    //alert("Now going to make POST ajax call - results will appear soon");
+    $.get("/products", function (data, status) {
         //obj = JSON.parse(data);
-        for(i = 0; i < data.length; i++) {
-            if(data[i].name == document.title) {
-                $("#bodytext").text(obj[i].description);
+        for (i = 0; i < data.length; i++) {
+            if (data[i].name == document.title) {
+                $("#bodytext").text(data[i].description);
             }
 
         }
-        
-});
 
-
-
-
-
-
-$("#order").click(function() {
-    sessionStorage.count = Number(sessionStorage.count) + 1;
-    $.get("/products", function(data,status) {
-        //obj = JSON.parse(data);
-        for(i = 0; i < data.length; i++) {
-            if(data[i].name == document.title) {
-                //$("#bodytext").text(obj[i].description);
-                sessionStorage.order += JSON.stringify(data[i]);
-            }
-        }
     });
-})
-//$("#tester").text("Testing is working");
+
+
+
+
+
+
+    $("#order").click(function () {
+        sessionStorage.count = Number(sessionStorage.count) + 1;
+        $.get("/products", function (data, status) {
+            //obj = JSON.parse(data);
+            for (i = 0; i < data.length; i++) {
+                if (data[i].name == document.title) {
+                    //$("#bodytext").text(obj[i].description);
+                    sessionStorage.order += JSON.stringify(data[i]);
+                }
+            }
+        });
+    })
+    //$("#tester").text("Testing is working");
 
 
 }); // end of document ready function
