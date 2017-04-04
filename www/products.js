014f686bdc9b775f25bd826b3fc1a77bd2783600
Nicus Hicks <nphicks@iastate.edu>
2017-04-03 21:27:17 -0500

    hoping to finish soon

--- www/products.js
+++ www/products.js
@@ -5,8 +5,11 @@ $(document).ready(function() {
 
 // the code is inside the ready function - 
 // makes sure that DOM is loaded before using jQuery
-;
 
+if(!sessionStorage.order && !sessionStorage.count)
+{
+    sessionStorage.count = 0;
+}
 
   //alert("Now going to make POST ajax call - results will appear soon");
 $.get("/products", function(data,status) {
@@ -20,7 +23,25 @@ $.get("/products", function(data,status) {
         
 });
 
-$("#tester").text("Testing is working");
+
+$("#cartcount").text(sessionStorage.count);
+
+
+
+$("#order").click(function() {
+    $.get("/products", function(data,status) {
+        //obj = JSON.parse(data);
+        for(i = 0; i < data.length; i++) {
+            if(data[i].name == document.title) {
+                //$("#bodytext").text(obj[i].description);
+                sessionStorage.setItem("order", JSON.stringify(data[i]));
+                sessionStorage.count += 1;
+            }
+        }
+    });
+})
+//$("#tester").text("Testing is working");
+
 
 }); // end of document ready function
 
